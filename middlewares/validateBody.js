717d4e8b43d243260aab4aef3a33fe39c3babfd3
TanyaHoncharova <tanya.honcharova@mapotic.com>
2023-07-22 16:58:04 +0200

    chore(clean)

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = schemas => {
     const func = (req, res, next) => {
         const { error } = schemas.validate(req.body);
         if(error){
-            next(HttpError(400, error.massage));
+            next(HttpError(400, error.message));
         }
         next()
     }
